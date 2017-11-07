9e9d8980c936260134c30839d262871ae881474f
BReily89 <BrandonReily.br@gmail.com>
2017-11-07 14:49:35 -0500

    again

--- app/controllers/api/creatures_controller.rb
+++ app/controllers/api/creatures_controller.rb
@@ -1,36 +1,38 @@
 class Api::CreaturesController < ApplicationController
-    def create 
-        creature_params = params.require(:creature).permit(:name, :description)
-        @creature = Creature.create!(creature_params)
-        render json: @creature
-    def index @creatures = Creature.ApplicationController
-        render json: @Creature.all
-        if @creature.save
-            render json: @creature
-    end
- def show   
-    creature_id = params[:id]
     
-    @creature = Creature.find_by_id(creature_id)
-    render json: @creature
-    end
-def update
-    creature_id = params[:id]
-    @creature = Creature.find_by_id(creature_id)
-    creature_params = params.require(:creature).permit(:name, :description
-    @creature.update_attributes(creature_params)
-    render json: @creature
-end
-def destroy
-    creature_id = params[:id]
-    @creature = Creature.find_by_id(creature_id)
-    @creature.destroy(creature_params)
-    render json: {
-        msg: "peace"
-    }
-end
-private 
-def creature_paramsparams.require(:creature).permit(:name, :description)
-end
-    
-end
+       def index
+           @creatures = Creature.all
+           render json: @creatures
+       end
+       def create
+            @creature = Creature.new(creature_params)
+            if @creature.save
+               render json: @creature
+            end
+       end
+        def show
+           creature_id = params[:id]
+           @creature = Creature.find_by_id(creature_id)
+           render json: @creature
+       end
+       def update
+           creature_id = params[:id]
+           @creature = Creature.find_by_id(creature_id)
+         
+           @creature.update_attributes(creature_params)
+           render json: @creature
+       end
+       def destroy
+           creature_id = params[:id]
+           @creature = Creature.find_by_id(creature_id)
+           @creature.destroy
+           render json: {
+         msg: "Successfully Deleted"
+       }
+       end
+       private
+       def creature_params
+             params.require(:creature).permit(:name, :description)
+       end
+   end
+   
